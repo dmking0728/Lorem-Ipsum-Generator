53a830b68c15d7d99de69d15f5dc0cf1a370da55
daniel king <danielking@daniels-MacBook-Pro.local>
2019-06-27 20:02:25 -0400

    Updated UI

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+//define UI elements i want to work with
 const inputNumber = document.querySelector('select');
 const generateButton = document.querySelector('input');
 const clearButton = document.getElementById('clear');
 const copyButton = document.getElementById('copy');
 
+//add Eevent listeners to buttons
 generateButton.addEventListener('click', calcNumOfLorem);
 clearButton.addEventListener('click', clearStuff);
 copyButton.addEventListener('click', copyTo);
 
+//have a const that cntains 1 paragraph of Lorem Ipsum 
 const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
+
+//calucaltes the # of LI paragraphs to display
 function calcNumOfLorem(e){
   const selected = inputNumber.value;
+  //Once a user clicks generate the button becomes diasbled to stop any weird bugs from happening
   generateButton.setAttribute("disabled", "disabled");
 
   for(i = 0; i < selected; i++){
@@ -21,6 +27,7 @@ function calcNumOfLorem(e){
   }
 }
 
+//clear the generated paragraphs off the screen
 function clearStuff(e){
   const p = document.querySelectorAll('.lorem');
 
@@ -31,10 +38,11 @@ function clearStuff(e){
     p.forEach((e) =>{
     e.remove();
     });
-  }
-  
+  } 
 }
 
+
+//copies the generated paragraphs to the user's clipboard
 function copyTo(){
   const copyLorems = document.querySelectorAll('.lorem');
 
@@ -45,15 +53,12 @@ if (copyLorems.length === 0){
   for(i = 0; i < copyLorems.length; i++){
     html += copyLorems[i].innerHTML;
   }
-
-  const hiddenInput = document.createElement('input');
-  document.body.appendChild(hiddenInput);
-  hiddenInput.id = 'hidden';
-  hiddenInput.value = html;
-  hiddenInput.select();
-  document.execCommand("copy", false);
-  alert('Copied!');
-}
-
-  
+    const hiddenInput = document.createElement('input');
+    document.body.appendChild(hiddenInput);
+    hiddenInput.id = 'hidden';
+    hiddenInput.value = html;
+    hiddenInput.select();
+    document.execCommand("copy", false);
+    alert('Copied!');
+  }
 }
